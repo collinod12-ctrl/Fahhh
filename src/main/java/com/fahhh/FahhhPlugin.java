069167e20fdc5f7cd410e26169b3ed06311a01aa
collinod12 <collino12@gmail.com>
2026-05-09 11:54:14 -0400

    setup commit

--- src/main/java/com/fahhh/FahhhPlugin.java
+++ src/main/java/com/fahhh/FahhhPlugin.java
@@ -2,15 +2,22 @@ package com.fahhh;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.RuneLite;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
+import java.io.File;
 
 @Slf4j
 @PluginDescriptor(
@@ -22,13 +29,17 @@ public class FahhhPlugin extends Plugin
 	@Inject
 	private Client client;
 
-	@Inject
+    @Inject
 	private FahhhConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.debug("Fahhh started!");
+		File soundFile = new File(RuneLite.RUNELITE_DIR, "Fahhh.wav");
+		AudioInputStream stream = AudioSystem.getAudioInputStream(soundFile);
+        Clip deathSoundClip = AudioSystem.getClip();
+		deathSoundClip.open(stream);
 	}
 
 	@Override
